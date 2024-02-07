103947f2a32283f38357684e008131d8580290d4
RadhikaYerramsetty <150806644+RadhikaYerramsetty@users.noreply.github.com>
2024-02-07 20:08:56 +0530

    push my local code to repo

---
+++ server/db/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const connectDB = () => {
+  return mongoose.connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+};
+module.exports = connectDB;
